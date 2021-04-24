86fe656345e6ac23e60e7fe2e9c57f9577784421
Noah Chongsiriwatana <noah@chongsiriwatana.net>
2021-04-24 18:46:17 -0400

    Added documentation.

--- WellSolver.cpp
+++ WellSolver.cpp
@@ -1,3 +1,13 @@
+/*
+ * Author: Noah Chongsiriwatana
+ *
+ * Solves 2d infinite square well with elliptical boundary conditions.
+ *
+ * TODO: Use numerical methods to properly solve the elliptical boundary conditions,
+ * 		Rather than by juxtaposing solutions to circular boundary conditions.
+ * 		Try using boundary element method as described in link found in README.
+ */
+
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <Python.h>
@@ -10,16 +20,28 @@
 namespace py = pybind11;
 using namespace std;
 
+/*
+ * Porting function to generate points according to probability distribution.
+ * 
+ * Returns contiguous data and metadata to be interpreted by numpy.
+ */
 py::array_t<double> solve(double a, double b, int n, int e1, int e2) {
+	
+	/*
+	 * Initialize data to random points.
+	 * This is necessary because a variable number of points will be generated based on probability distribution,
+	 * but pybind11 demands that a set memory structure be defined first.
+	 */
 	random_device rand;
 	default_random_engine eng(rand());
 	uniform_real_distribution<double> dist_a(-a, a);
 	uniform_real_distribution<double> dist_b(-b, b);
-	double h = 1000.;
-	double hx = 2*a/h;
-	double hy = 2*b*h/n;
 	int precision = 10;
 
+	/*
+	 * Numpy compatible data must be carefully constructed as an array 
+	 * with the size of the product of the shapes of the intended numpy array.
+	 */
 	double *data = new double[n*2*precision];
 	
 	for (int i = 0; i < 2*n*precision; ++i) {
@@ -27,16 +49,38 @@ py::array_t<double> solve(double a, double b, int n, int e1, int e2) {
 		data[i+1] = dist_b(eng);
 	}
 
+	/*
+	 * Defines step sizes.
+	 */
+	double h = 1000.;
+	double hx = 2*a/h;
+	double hy = 2*b*h/n;	
+
+	/*
+	 * Generates points based on probability distribution.
+	 */
 	int i = 0;
 	for (int x_index = 0; x_index < 2*a/hx; ++x_index) {
 		for (int y_index = 0; y_index < 2*b/hy; ++y_index) {
+
+			/*
+			 * At each new (x, y) pair, generate points.
+			 */
 			double x = -a + x_index*hx;
 			double y = -b + y_index*hy;
 			if (potential(a, b, x, y) < 1) {
+				
+				/*
+				 * Calculates probability density and generates associated number of points.
+				 */
 				double weight = wavefunction(a, b, x, y, e1, e2);
 				double prob_density = weight * weight;
 				for (int j = 0; j < precision; ++j) {
 					if (double(j)/double(precision) < prob_density && i < 2*n*precision) {
+
+						/*
+						 * Every other value in array contains x and y values based on the shape definitions.
+						 */
 						data[i] = x;
 						data[i+1] = y;
 						i += 2;
@@ -46,11 +90,18 @@ py::array_t<double> solve(double a, double b, int n, int e1, int e2) {
 		}
 	}
 
+	/*
+	 * Pybind11 requires explicit control over memory.
+	 * This function is called when Python expects the garbage collector to free the numpy array.
+	 */
 	py::capsule free_when_done(data, [](void *f) {
 		double *data = reinterpret_cast<double *>(f);
 		delete[] data;
 	});
 	
+	/*
+	 * Stores and returns data, metadata, and destructor function to help python access/destroy memory.
+	 */
 	auto result = py::array_t<double>(
 		{n, 2*precision},
 		{2*precision*sizeof(double), sizeof(double)},
@@ -60,11 +111,17 @@ py::array_t<double> solve(double a, double b, int n, int e1, int e2) {
 	return result;
 }
 
+/*
+ * Binds some light documentation with the solve function to be exported as a python compatible module.
+ */
 PYBIND11_MODULE(WellSolver, m) {
 	m.doc() = "Python binding for WellSolver.";
 	m.def("solve", &solve, "solve a 2D Well problem numerically.");
 }
 
+/*
+ * Represents infinite elliptical well with 0 inside and a large number outside.
+ */
 double potential(double a, double b, double x, double y) {
 	if (x*x/double(a*a)+y*y/double(b*b) < 1.) {
 		return 0;
@@ -72,20 +129,20 @@ double potential(double a, double b, double x, double y) {
 	return numeric_limits<double>::max();
 }
 
+/*
+ * Calculates the wavefunction using the spherical harmonics and bessels functions.
+ * 
+ * This technique is imperfect for elliptical bounds, especially at high eccentricity.
+ * TODO: Use numerical methods to properly solve this problem.
+ */
 double wavefunction (double a, double b, double x, double y, int e1, int e2) {
-	//return sin(e1*M_PI*x/x_length(a, b, y))*sin(e2*M_PI*y/y_length(a, b, x));
 	double theta = atan2(y, x);
 	return radial(a, b, sqrt(x*x+y*y), theta, e1, e2) * angular(theta, e2);
 }
 
-double x_length (double a, double b, double y) {
-	return a*sqrt(1-y*y/(b*b));
-}
-
-double y_length (double a, double b, double x) {
-	return b*sqrt(1-x*x/(a*a));
-}
-
+/*
+ * Calculates the radial component of wavefunction.
+ */
 double radial (double a, double b, double r, double theta, int z, int l) {
 	double x_rel = a*sin(theta);
 	double y_rel = b*cos(theta);
@@ -94,6 +151,9 @@ double radial (double a, double b, double r, double theta, int z, int l) {
 	return N*jn(l, z*r/r_rel);
 }
 
+/*
+ * Calculates angular component of wavefunction.
+ */
 double angular (double theta, int l) {
 	double N = pow((l+1), 3);
 	return N*cos(theta*l);
--- wellgraphics.py
+++ wellgraphics.py
@@ -1,5 +1,12 @@
-# Module for well application components.
+# Author: Noah Chongsiriwatana
+#
+# Module building UI to solve 2d infinite elliptical well.
+#
+# TODO: Find more efficient way to plot 2d heatmaps without generating many points.
+# 		Investigate more seaborn tools, or try other libraries.
 
+
+import numpy
 import re
 import math
 import tkinter as tk
@@ -8,19 +15,25 @@ import seaborn as sns
 import WellSolver
 import pandas
 import pathlib
-import numpy
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib import patches
 
+# Definition for Matplotlib based embedded plot.
 class WellPlot:
 	def __init__(self, window, a, b, energy_level_1, energy_level_2, n):
+		
+		# Initializes length scales of plot.
 		self.a = a
 		self.b = b
+
+		# Initializes resolution and initial quantum numbers.
 		self.n = n
 		self.energy_level_1 = energy_level_1
 		self.energy_level_2 = energy_level_2
+
+		# Builds the Matplotlib window and two empty figures, then embed it in the tk GUI.
 		self.window = window
 		self.figure = Figure(figsize=(6, 6), dpi=100)
 		self.plot = self.figure.add_subplot(2, 1, 1)
@@ -28,63 +41,83 @@ class WellPlot:
 		self.canvas = FigureCanvasTkAgg(self.figure, window)
 		self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
+	# Updates the figures based on new parameters.
 	def replot(self, a, b, energy_level_1, energy_level_2):
+
+		# Updates parameters.
 		self.a = a
 		self.b = b
 		self.energy_level_1 = energy_level_1
 		self.energy_level_2 = energy_level_2
+
+		# Removes old figures, then rebuilds from new parameters.
 		self.plot.clear()
 		self.density_plot.clear()
 		self.plot.set_title("Probability Density, a=" + str(a) + ", b=" + str(b) + ", n1,n2=" + str(energy_level_1) + "," + str(energy_level_2))
+
+		# Draws ellipse and finds x-y boundaries.
 		self.bounds = patches.Ellipse((0, 0), 2*a, 2*b, edgecolor='black', facecolor='none')
 		self.plot.add_patch(self.bounds)
 		bound = WellPlot.find_bound(a, b)
 		self.plot.set_xlim([-bound, bound])
 		self.plot.set_ylim([-bound, bound])
+
+		# Plots the probability density and finally redraws the figures.
 		self.plot_density()
 		self.canvas.draw()
 		self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
+	# Plots the appropriate probability density.
 	def plot_density(self):
-		x = self.a * numpy.random.normal(size=self.n)
-		y = x * self.b + numpy.random.normal(size=self.n)
+		
+		# Uses C++ based module to generate points based on a probability distribution.
 		data = WellSolver.solve(self.a, self.b, int(self.n), int(self.energy_level_1), int(self.energy_level_2))
 		data = numpy.copy(numpy.transpose(data))
 		x = data[0]
 		y = data[1]
-		self.density_plot.hist2d(x, y, bins=(100, 100), cmap=matplotlib.pyplot.cm.jet)
-		#self.plot.plot(x, y, 's')
 
+		# Generates 2d histogram of points.
+		self.density_plot.hist2d(x, y, bins=(100, 100), cmap=matplotlib.pyplot.cm.jet)
+
+	# Finds bounds for plotting.
 	@staticmethod
 	def find_bound(a, b):
 		high = max(a, b)
 		return 1.1*high
 
+# Builds a UI to change the parameters of the well and to change the quantum numbers.
 class WellUI:
-	# Class variables.
+
+	# Classwide style variables.
 	title_font = "Helvetica 18 bold"
 	plain_font = "Helvetica 16"
 	
 	def __init__(self):
-		# Initial parameters.
+
+		# Initial parameters, quantum numbers, and resolution.
 		self.a = 1
 		self.b = 1
 		self.energy_level_n1 = 1
 		self.energy_level_n2 = 0
 		self.resolution = 100000;
+
 		# Creates window.
 		self.window = tk.Tk()
+		
 		# Creates title.
 		self.frm_title = tk.Frame()
 		self.lbl_title = tk.Label(master=self.frm_title, text="2d Particle in a Box Simulation", font=WellUI.title_font)
 		self.lbl_title.pack()
-		# Creates plot.
+		
+		# Creates new embedded Matplotlib figures.
 		self.plot_canvas = WellPlot(self.window, self.a, self.b, self.energy_level_n1, self.energy_level_n2, self.resolution)
+		
 		# Creates ui for ellipse parameters.
 		self.frm_ellipse = tk.Frame(borderwidth=2, relief="solid")
 		self.lbl_eqn = tk.Label(master=self.frm_ellipse, text="(x/a)^2 + (y/b)^2 = 1", font=WellUI.plain_font)
 		self.lbl_eqn.pack()
-		# Creates entry fields.
+		
+		# Creates entry fields and sliders for parameters and quantum numbers respectively.
 		self.a_entry = tk.Entry(master=self.frm_ellipse)
 		self.b_entry = tk.Entry(master=self.frm_ellipse)
 		self.level_slider_n1 = tk.Scale(
@@ -101,10 +134,14 @@ class WellUI:
 			orient=tk.HORIZONTAL,
 			command=self.apply_energy
 		)
+
+		# Creates input labels.
 		self.lbl_a = tk.Label(master=self.frm_ellipse, text="Enter a:")
 		self.lbl_b = tk.Label(master=self.frm_ellipse, text="Enter b:")
 		self.lbl_level_n1 = tk.Label(master=self.frm_ellipse, text="Set 1st Energy:")
 		self.lbl_level_n2 = tk.Label(master=self.frm_ellipse, text="Set 2nd Energy:")
+
+		# Packs all components in order into the tk window.
 		self.lbl_a.pack()
 		self.a_entry.pack()
 		self.lbl_b.pack()
@@ -113,7 +150,8 @@ class WellUI:
 		self.level_slider_n1.pack()
 		self.lbl_level_n2.pack()
 		self.level_slider_n2.pack()
-		# Creates submit button.
+
+		# Creates and packs submit button.
 		self.frm_submit = tk.Frame()
 		self.btn_submit = tk.Button(
 			master=self.frm_submit,
@@ -121,13 +159,17 @@ class WellUI:
 			command=self.apply_params
 		)
 		self.btn_submit.pack()
+
 		# Packs frames and initializes window.
 		self.frm_title.pack()
 		self.frm_ellipse.pack()
 		self.frm_submit.pack()
 		self.window.mainloop()
 
+	# Callback response to apply new parameters to plot.
 	def apply_params(self, energy_level=0):
+
+		# Updates length parameters and quantum numbers.
 		self.a = max(1, WellUI.to_float(self.a_entry.get()))
 		self.b = max(1, WellUI.to_float(self.b_entry.get()))
 		higher = max(self.a, self.b)
@@ -138,15 +180,22 @@ class WellUI:
 		self.energy_level_n2 = self.level_slider_n2.get()
 		self.a_entry.delete(0, tk.END)
 		self.b_entry.delete(0, tk.END)
+
+		# Replots both figures.
 		self.plot_canvas.replot(self.a, self.b, self.energy_level_n1, self.energy_level_n2)
 
+	# Callback response to apply new quantum numbers to plot.
 	def apply_energy(self, energy_level):
+
+		# Updates quantum numbers and applies to plot.
 		self.energy_level_n1 = self.level_slider_n1.get()
 		self.energy_level_n2 = self.level_slider_n2.get()
 		self.plot_canvas.replot(self.a, self.b, self.energy_level_n1, self.energy_level_n2)
 
 	# Static Methods.
 	@staticmethod
+
+	# Extracts valid floating point number from arbitrary input text.
 	def to_float(s):
 		val = 0
 		finds = re.findall(r"[-+]?\d*\.\d+|\d+", s)
